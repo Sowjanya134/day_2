dfb76fc6dcc7c3407b1220d2e3a071f784068d1e
Sowjanya134 <sowjanyabathuni405@gmail.com>
2025-07-01 15:48:31 +0530

    assignments

---
+++ assdiv.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int scalary;
+	printf("enter the scalary");
+	scanf("%d",&scalary);
+	printf("the scalary:%d",scalary);
+	scalary|=21;
+	scanf("the scalary:%d",scalary);
+}
